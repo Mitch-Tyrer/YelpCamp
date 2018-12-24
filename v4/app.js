b60358450abb894a95269cf6d3d482b24f28df99
Mitchell Tyrer <mltcinematography@gmail.com>
2018-12-24 23:32:50 +0000

    12242018

---
+++ v4/app.js
@@ -0,0 +1,76 @@
+var express = require("express"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    seedDB = require("./seeds");
+    
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", { useNewUrlParser: true });
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+seedDB();
+
+
+app.get("/", function(req, res){
+    res.render("home");
+});
+
+app.get("/campgrounds", function(req, res){
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+        }
+    });
+       
+});
+
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("campgrounds/new");
+});
+
+
+app.get("/campgrounds/:id", function(req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+     if(err){
+         console.log(err);
+     }   else {
+         console.log(foundCampground);
+         res.render("campgrounds/show", {campground: foundCampground});
+     }
+    });
+});
+
+// ======================
+// COMMENTS
+// ======================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground});
+        }
+    });
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("server is a go!");
+});
