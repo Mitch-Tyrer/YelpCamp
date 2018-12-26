74290a684b9a90a99a2ea1ea186509e0f99bcde2
Mitchell Tyrer <mltcinematography@gmail.com>
2018-12-26 20:57:49 +0000

    12262018

---
+++ v9/routes/campgrounds.js
@@ -0,0 +1,83 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+//INDEX - Show all campgrounds
+router.get("/", function(req, res){
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/index", {campgrounds: allCampgrounds});
+        }
+    });
+       
+});
+
+
+router.post("/", isLoggedIn, function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: description, author: author};
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+});
+
+router.get("/new", isLoggedIn, function(req, res){
+    res.render("campgrounds/new");
+});
+
+
+router.get("/:id", function(req, res) {
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+     if(err){
+         console.log(err);
+     }   else {
+         console.log(foundCampground);
+         res.render("campgrounds/show", {campground: foundCampground});
+     }
+    });
+});
+
+//edit campground route
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            res.redirect("/campgrounds")
+        } else {
+            res.render("campgrounds/edit", {campground: foundCampground}); 
+        }
+    })
+});
+//update campground route
+router.put("/:id", function(req, res){
+   //find and update correct campground
+   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err){
+           res.redirect("/campgrounds");
+       } else {
+              //redirect to showpage
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+   })
+
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/login');
+}
+
+
+module.exports = router;
