e02ee4f2ee524e673559b651de464b635b813012
Mitchell Tyrer <mltcinematography@gmail.com>
2018-12-26 20:27:26 +0000

    12262018

--- v8/routes/campgrounds.js
+++ v8/routes/campgrounds.js
@@ -14,11 +14,15 @@ router.get("/", function(req, res){
 });
 
 
-router.post("/", function(req, res){
+router.post("/", isLoggedIn, function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: description, author: author};
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
