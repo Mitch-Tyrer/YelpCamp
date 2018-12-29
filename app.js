b537e18770ee10f2c4d55a64020db9f6ff50fab8
Mitchell Tyrer <mltcinematography@gmail.com>
2018-12-29 19:31:04 +0000

    updated db variables

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v9", { useNewUrlParser: true });
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v9";
+
+mongoose.connect(url, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
